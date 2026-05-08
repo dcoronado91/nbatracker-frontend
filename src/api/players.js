99433b9315bc1c00cb048d0251004b15cdf6f4ed
dcoronado91 <derekcoronado9@gmail.com>
2026-05-07 20:07:50 -0600

    feat: aádir Dockerfile y configuracion de nginx, actualizar API URL

--- src/api/players.js
+++ src/api/players.js
@@ -1,4 +1,4 @@
-const API_URL = "https://nbatracker-backend-production.up.railway.app";
+const API_URL = import.meta.env.VITE_API_URL || "https://nbatracker-backend-production.up.railway.app";
 
 export async function getPlayers(page = 1, limit = 9) {
     const res = await fetch(`${API_URL}/players?page=${page}&limit=${limit}`);
--- src/api/teams.js
+++ src/api/teams.js
@@ -1,4 +1,4 @@
-const API_URL = "https://nbatracker-backend-production.up.railway.app";
+const API_URL = import.meta.env.VITE_API_URL || "https://nbatracker-backend-production.up.railway.app";
 
 export async function getTeams() {
     const res = await fetch(`${API_URL}/teams`);
